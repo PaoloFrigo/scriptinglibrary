0550fed766a9aec2354ef8b0371e30fd5003086d
Paolo Frigo <paolofrigo@gmail.com>
2017-10-02 12:57:50 +1100

    Test-AdCredential script added.

---
+++ Blog/Test-AdCredential.ps1
@@ -0,0 +1,18 @@
+#
+#   Paolo Frigo, www.scriptinglibrary.com
+#
+
+function Test-ADCredential($cred) {
+    try {
+        start-process 'cmd' -Credential $cred    
+        $auth_success = [bool](Get-Process 'cmd' -IncludeUserName | Select-Object id, username, processname )
+        if ($auth_success -eq $true) {       
+            Get-Process 'cmd' -IncludeUserName | Where-Object {$_.username -eq "$($cred.Username)" } | Stop-Process -Force
+            Write-OutPut "Autentication succedeed for $($cred.Username)"
+        }   
+    }   
+    catch {
+        Write-Error "Authentication Failed for $($cred.Username)"
+    }
+}
+Test-ADCredential($(Get-Credential))
