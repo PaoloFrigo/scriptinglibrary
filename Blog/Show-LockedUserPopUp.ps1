1a078f63b006dca1a1044c10792b923b9aef393b
Paolo Frigo <paolofrigo@gmail.com>
2017-10-23 17:51:43 +1100

    Added Show-LockedUserPopUp.ps1 script

---
+++ Blog/Show-LockedUserPopUp.ps1
@@ -0,0 +1,18 @@
+#
+#   Paolo Frigo, www.scriptinglibrary.com
+#
+header="".ToUpper()
+$footer=" This message will close automatically in $TimeOut seconds.".ToUpper()
+$NameList = ""
+$TimeOut = 15 #seconds
+ 
+$LockedOutUsersList = Search-ADAccount -LockedOut | Where-Object {$_.enabled -eq $true}
+ 
+if ($LockedOutUsersList.Length -gt 0){
+    foreach ($AdUser in $LockedOutUsersList){
+        $NameList += "$AdUser, "
+    }
+    $wshell = New-Object -ComObject Wscript.Shell
+    $wshell.Popup($header+$NameList+$footer,$TimeOut,"AD USERS LOCKED OUT",0)
+}
+exit 0
