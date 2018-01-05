1f1be50c5b405af8f521aaba2858d71084f4a388
Paolo Frigo <paolofrigo@gmail.com>
2018-01-05 17:11:03 +1100

    Modified Get-LocalAdmin script

---
+++ Blog/Get-LocalAdmin.ps1
@@ -0,0 +1,69 @@
+function Get-LocalAdmin {  
+ 
+    <#
+        
+        .Synopsis
+            Get Local admin list
+        .Description 
+            Get Local admin list
+ 
+        .Example
+            Get-LocalAdmin  -Computername myworkstation.contoso.com
+ 
+            This shows the NTP Status of the localhost, this will be the result:
+ 
+            Retrieving Local Admin list for myworkstation.contoso.com
+            MYWORKSTATION\Administrator           
+            CONTOSO\Domain Admins
+ 
+        .Example 
+             get-adcomputer -searchbase ‘OU=workstations,dc=contoso,dc=com’ -filter * -property * | select name  | Get-LocalAdmin
+ 
+             Get Local admin list for all the workstation in AD.
+ 
+        .Notes
+              Author: Paolo Frigo - paolofrigo@gmail.com
+ 
+ #>
+    param (
+ 
+        [Parameter(Mandatory = $true,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            Position = 0)]
+        [Alias('Name')]
+ 
+        [string[]]$ComputerName
+    )             
+    Process {
+           
+        Write-Warning "Retrieving Local Admin list for $ComputerName"   
+ 
+        try {
+            If (!(Test-Connection -ComputerName $computerName -Count 1 -Quiet)) { 
+                Write-Output "$computerName is offline."
+                #Continue # Move to next computer
+            }
+            else {
+                $admins = Gwmi win32_groupuser –computer $ComputerName 
+                $admins = $admins |? {$_.groupcomponent –like '*"Administrators"'}    
+                $admins | % {  
+                    $_.partcomponent –match “.+Domain\=(.+)\,Name\=(.+)$” > $nul  
+                    $matches[1].trim('"') + “\” + $matches[2].trim('"')  }
+            }
+            
+        }
+        catch {
+            Write-Output "Can't gather information from $ComputerName"
+            Write-Output $Error[0].Exception;
+        }
+        finally {
+           
+        } 
+      
+    }
+}
+ 
+ 
+#Example with desktop, but you can use Laptops or VMs or Servers as OU
+get-adcomputer -searchbase ‘OU=workstations,dc=contoso,dc=com’ -filter 
