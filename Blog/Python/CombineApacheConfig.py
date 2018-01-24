832a942513024711f23d13293fedc0c8ef66885e
Paolo Frigo <paolofrigo@gmail.com>
2018-01-24 15:04:30 +1100

    Code Refectored for combineapacheconfig.py

--- Blog/Python/CombineApacheConfig.py
+++ Blog/Python/CombineApacheConfig.py
@@ -1,82 +1,95 @@
-#!/usr/bin/python3
-# CombineApacheConfig.py 
-
+#!/usr/bin/python
+# CombineApacheConfig.py
+""" This script combines your APACHE httpd.conf with all included files\
+   into one and redirects it to the standard output. """
 __author__ = 'ben', 'PAOLO FRIGO, paolofrigo@gmail.com | www.scriptinglibrary.com'
-import sys, os, os.path, logging, fnmatch, re, argparse
 
-def ProcessMultipleFiles(InputFiles):
-    if InputFiles.endswith('/'):              
-        InputFiles = InputFiles + "*"
-    Content = ''
-    LocalFolder = os.path.dirname(InputFiles)
-    basenamePattern = os.path.basename(InputFiles)
-    for root, dirs, files in os.walk(LocalFolder):
-        for filename in fnmatch.filter(files, basenamePattern):
-            Content += ProcessInput(os.path.join(root, filename))
-    return Content
+import os
+import os.path
+import logging
+import fnmatch
+import re
+import argparse
 
 
-def RemoveExcessiveLinebreak(LineOfContent):
-    Length = len(LineOfContent)
-    LineOfContent = LineOfContent.replace(os.linesep + os.linesep + os.linesep, os.linesep + os.linesep)
-    NewLength = len(LineOfContent)
-    if NewLength < Length:
-        LineOfContent = RemoveExcessiveLinebreak(LineOfContent)
-    return LineOfContent
+def ProcessMultipleFiles(inputfiles):
+    """ Process an expression with /* with all the files included on that directory """
+    if inputfiles.endswith('/'):
+        inputfiles = inputfiles + "*"
+    content = ''
+    localfolder = os.path.dirname(inputfiles)
+    basenamepattern = os.path.basename(inputfiles)
+    for root, dirs, files in os.walk(localfolder):
+        for filename in fnmatch.filter(files, basenamepattern):
+            content += ProcessInput(os.path.join(root, filename))
+    return content
 
 
-def ProcessInput(InputFile):
-    global ServerRoot
+def RemoveExcessiveLinebreak(lineofcontent):
+    """ Remove Excessive Linebreaks form the line of content passed as argument """
+    length = len(lineofcontent)
+    lineofcontent = lineofcontent.replace(
+        os.linesep + os.linesep + os.linesep, os.linesep + os.linesep)
+    newlength = len(lineofcontent)
+    if newlength < length:
+        lineofcontent = RemoveExcessiveLinebreak(lineofcontent)
+    return lineofcontent
 
-    Content = ''
+
+def ProcessInput(inputfile):
+    """ This function accepts an input path for the httpd conf file, searchs
+        for 'include' and replaces it with the matchin content of the included
+        file, add start and end comments and remove all other comments or
+        spaces. """
+    content = ''
     if logging.root.isEnabledFor(logging.DEBUG):
-        Content = '# Start of ' + InputFile + os.linesep
-    with open(InputFile, 'r') as infile:
+        content = '# Start of ' + inputfile + os.linesep
+    with open(inputfile, 'r') as infile:
         for line in infile:
             stripline = line.strip(' \t')
             if stripline.startswith('#'):
                 continue
-            searchroot = re.search(r'ServerRoot\s+(\S+)', stripline, re.I)      #search for ServerRoot
+            # search for serverroot
+            searchroot = re.search(r'serverroot\s+(\S+)', stripline, re.I)
             if searchroot:
-                ServerRoot = searchroot.group(1).strip('"')
-                logging.info("ServerRoot: " + ServerRoot)
+                serverroot = searchroot.group(1).strip('"')
+                logging.info("serverroot: " + serverroot)
             if stripline.lower().startswith('include'):
                 match = stripline.split()
                 if len(match) == 2:
-                    IncludeFiles = match[1]
-                    IncludeFiles = IncludeFiles.strip('"') 
-                    if not IncludeFiles.startswith('/'):
-                        IncludeFiles = os.path.join(ServerRoot, IncludeFiles)
-                    Content += ProcessMultipleFiles(IncludeFiles) + os.linesep
+                    includefiles = match[1]
+                    includefiles = includefiles.strip('"')
+                    if not includefiles.startswith('/'):
+                        includefiles = os.path.join(serverroot, includefiles)
+                    content += ProcessMultipleFiles(includefiles) + os.linesep
                 else:
-                    Content += line
+                    content += line
             else:
-                Content += line
-    Content = RemoveExcessiveLinebreak(Content)
+                content += line
+    content = RemoveExcessiveLinebreak(content)
     if logging.root.isEnabledFor(logging.DEBUG):
-        Content += '# End of ' + InputFile + os.linesep + os.linesep
-    return Content
+        content += '# End of ' + inputfile + os.linesep + os.linesep
+    return content
+
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG, format='[%(asctime)s][%(levelname)s]:%(message)s')
-    parser=argparse.ArgumentParser(
-    description="""DESCRIPTION: This script will combine your APACHE httpd.conf with all included files into one.""",
-    epilog="""Author :Ben, Paolo Frigo""")
-    parser.add_argument('--apacheconf', required=True, default="/etc/httpd/conf/httpd.conf", help='SPECIFY YOUR HTTPD/APACHE CONF FILE PATH.  i.e. /etc/httpd/conf/httpd.conf')
-    parser.add_argument('--combinedconf', required=True, default="/tmp/httpd.combined.conf", help='SPECIFY YOUR OUTPUT FILE. i.e. /tmp/httpd.combined.conf')
-    args=parser.parse_args()
-
+    logging.basicConfig(level=logging.DEBUG,
+                        format='[%(asctime)s][%(levelname)s]:%(message)s')
+    PARSER = argparse.ArgumentParser(
+        description="""DESCRIPTION: This script combines your APACHE httpd.conf with
+                    all included files into one and redirects it to the standard output.""",
+        usage="./combineapacheconfig.py /etc/httpd/conf/httpd.conf \
+              ./combineapacheconfig.py -h  ",
+        epilog="""Author :Ben, Paolo Frigo""")
+    PARSER.add_argument('apacheconf', default="/etc/httpd/conf/httpd.conf",
+                        help='SPECIFY YOUR HTTPD/APACHE CONF FILE PATH. \
+                            i.e. /etc/httpd/conf/httpd.conf')
+    USERSARGS = PARSER.parse_args()
     try:
-        ServerRoot = os.path.dirname(args.apacheconf)
-        Content = ProcessInput(args.apacheconf)
-    except Exception as e:
-        logging.error("Failed to process " + args.apacheconf,  exc_info=True)
+        serverroot = os.path.dirname(USERSARGS.apacheconf)
+        content = ProcessInput(args.apacheconf)
+    except FileNotFoundError as e:
+        logging.error("Failed to process " +
+                      USERSARGS.apacheconf, exc_info=True)
         exit(1)
-    try:
-        with open(args.combinedconf, 'w') as outfile:
-            outfile.write(Content)
-    except Exception as e:
-        logging.error("Failed to write to " + outfile,  exc_info=True)
-        exit(1)
-
-    logging.info("All files have been merged into : " + args.combinedconf)
+    print(content)
