dc1ffbdfcc9700251761f31b3aee9c638cd630c7
Paolo Frigo <paolofrigo@gmail.com>
2018-02-02 13:39:48 +1100

    added a batch script and modified the description of the repository.

---
+++ Blog/Batch/mdconvert.bat
@@ -0,0 +1,22 @@
+:: MD CONVERT
+:: ------------------------
+:: Converts all MarkDowns files passed as arguments into different 
+:: formats and saves them into a separate folder.
+::
+:: AUTHOR:              Paolo Frigo, paolofrigo@gmail.com 
+::                      https://www.scriptinglibrary.com
+::
+:: REQUIREMENTS:    
+::      pandoc (https://pandoc.org/)
+::      miktex for pdf conversion form LaTeX (https://miktex.org/)
+set list=docx html rst odt rtf epub pdf
+
+for %%x in (%*) do (
+    mkdir output   
+    mkdir output\\%%~nx
+    for %%e in (%list%) do ( 
+        mkdir output\\_%%e
+        pandoc %%x -o output/_%%e/%%~nx.%%e
+        pandoc %%x -o output/%%~nx/%%~nx.%%e
+    )
+)
