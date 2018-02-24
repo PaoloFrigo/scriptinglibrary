2469e1ffd27638ed5bacf74cb7fd389949f6242e
Paolo Frigo <paolofrigo@gmail.com>
2018-02-24 14:43:42 +1100

    Edited wifi-devices.sh using sed insted of replace

--- Blog/Bash/wifi-devices.sh
+++ Blog/Bash/wifi-devices.sh
@@ -7,8 +7,8 @@
 
 # CONFIGURE ACCORDING TO YOUR NEEDS
 REPORT_FILENAME="scan-$(date +%Y-%m-%d).txt"
-WIFI_SUBNET="192.168.0" 
+WIFI_SUBNET="10.0.0" 
 
-nmap -sL $WIFI_SUBNET.* | grep -v "^Nmap scan report for $WIFI_SUBNET" | replace "Nmap scan report for " ""| tee $REPORT_FILENAME
+nmap -sL $WIFI_SUBNET.* | grep -v "^Nmap scan report for $WIFI_SUBNET" | sed -e 's/Nmap scan report for //g'| tee $REPORT_FILENAME
 echo "--- $REPORT_FILENAME has been created! ---"
 ​
