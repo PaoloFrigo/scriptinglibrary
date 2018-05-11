345cfd0c4a7de8cddbefe550beb2783b535e13f8
Paolo Frigo <paolofrigo@HSD-MacBook-Air.local>
2018-05-11 23:00:41 +1000

    Typos and notes edited to Compress-Folder.ps1 script

--- Blog/PowerShell/Compress-Folder.ps1
+++ Blog/PowerShell/Compress-Folder.ps1
@@ -4,16 +4,12 @@ function Compress-Folder {
     <#
     .SYNOPSIS
        
-    
     .DESCRIPTION
         
-    
     .PARAMETER FolderName
 
-    
     .EXAMPLE
-        
-        Compress-Folder -FolderName 
+        Compress-Folder  -FolderName  "/Users/paolofrigo/Documents/scriptinglibrary/Blog/PowerShell/tmp"
         
     .NOTES
         Author: paolofrigo@gmail.com, https://www.scriptinglibrary.com
@@ -21,22 +17,15 @@ function Compress-Folder {
     #>
         [CmdletBinding()]  # Add cmdlet features.
         Param (
-            # Define parameters below, each separated by a comma
-    
             [Parameter(Mandatory=$True)]
             [string]$FolderName,
             $destination = "$FolderName.zip"
-
-        )
+            )
     
         Begin {
-            # Start of the BEGIN block.
-            Write-Verbose -Message "Entering the BEGIN block [$($MyInvocation.MyCommand.CommandType): $($MyInvocation.MyCommand.Name)]."
-            Write-Verbose "Checking PowerShell Version"
             $PSVersion = $PSVersionTable.PSVersion.Major   
-            
         } 
-    
+
         Process {
             if ($PSVersion -gt 5 ){
                 Compress-Archive -Path $FolderName -DestinationPath $destination
@@ -49,10 +38,7 @@ function Compress-Folder {
                 [io.compression.zipfile]::CreateFromDirectory($FolderName, $destination) 
             }
         } 
-    
         End {
             Write-Verbose "Archive Created: $destination"
         } 
     }
-
-Compress-Folder  -FolderName  "/Users/paolofrigo/Documents/scriptinglibrary/Blog/PowerShell/tmp"
