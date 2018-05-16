b3731e8aceffada158f5b6a736c1bfb9694a4d59
Paolo Frigo <paolo.frigo@hsd.com.au>
2018-05-16 10:44:43 +1000

    Compress-Folder.ps1 script edited, new parameters  and exception handling.

--- Blog/PowerShell/Compress-Folder.ps1
+++ Blog/PowerShell/Compress-Folder.ps1
@@ -2,11 +2,13 @@
 
 function Compress-Folder {
     <#
-    .SYNOPSIS
+    .SYNOPSIS        
        
     .DESCRIPTION
         
     .PARAMETER FolderName
+
+    .PARAMETER RemoveDirWhenFinished
 
     .EXAMPLE
         Compress-Folder  -FolderName  "/Users/paolofrigo/Documents/scriptinglibrary/Blog/PowerShell/tmp"
@@ -15,30 +17,47 @@ function Compress-Folder {
         Author: paolofrigo@gmail.com, https://www.scriptinglibrary.com
     
     #>
-        [CmdletBinding()]  # Add cmdlet features.
-        Param (
-            [Parameter(Mandatory=$True)]
-            [string]$FolderName,
-            $destination = "$FolderName.zip"
-            )
-    
-        Begin {
-            $PSVersion = $PSVersionTable.PSVersion.Major   
-        } 
-
-        Process {
-            if ($PSVersion -gt 5 ){
-                Compress-Archive -Path $FolderName -DestinationPath $destination
-            } 
-            else {
-                If(Test-path $destination) {
-                    Remove-Item $destination -confirm
-                }
-                Add-Type -assembly "system.io.compression.filesystem"
-                [io.compression.zipfile]::CreateFromDirectory($FolderName, $destination) 
+    [CmdletBinding()]  # Add cmdlet features.
+    Param (
+        [Parameter(Mandatory = $True)]
+        [string]$FolderName,          
+        [Parameter(Mandatory = $False)]
+        [bool]$RemoveDirWhenFinished  
+    )    
+    Begin {
+        $PSVersion = $PSVersionTable.PSVersion.Major     
+        $FolderNameFullPath=(Resolve-Path $FolderName).Path        
+        $DestinationFullPath="$FolderNameFullPath.zip"
+        $Destination = "$Foldername.zip"       
+    } 
+    Process {
+        if (Test-Path -path $FolderNameFullPath) {           
+            If (Test-path $DestinationFullPath) {
+                Remove-Item $DestinationFullPath #-confirm
             }
-        } 
-        End {
-            Write-Verbose "Archive Created: $destination"
-        } 
-    }
+            try {
+                if (($PSVersion -ge 5) -and (Get-ChildItem -path $FolderNameFullPath).count -gt 0) {   
+                    Compress-Archive -Path $FolderNameFullPath -DestinationPath $DestinationFullPath -Force -CompressionLevel Optimal
+                } 
+                else {                                               
+                    Add-Type -assembly "system.io.compression.filesystem"                   
+                    [io.compression.zipfile]::CreateFromDirectory($FolderNameFullPath, $DestinationFullPath) 
+                }
+                Write-Verbose "Archive Created: $Destination"
+                if ((test-path -path $DestinationFullPath) -and $RemoveDirWhenFinished -eq $True) {
+                    Remove-Item -Recurse -path $FolderNameFullPath
+                    Write-Verbose "$FolderName removed."
+                }
+            }
+            catch {
+                Write-Error "Compression failed for $FolderName"                
+            }      
+        }
+        else {
+            Write-Error "Path not valid $FolderNameFullPath"
+        }   
+    } 
+    End {        
+
+    } 
+}
