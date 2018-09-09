073b1ea628364d0445f16684bd8dfcc06d8ee931
Paolo Frigo <paolofrigo@gmail.com>
2018-09-09 15:13:23 +1000

    Get-VMFootprintByEnv.ps1 script added

---
+++ Blog/PowerShell/Get-VMFootprintByEnv.ps1
@@ -0,0 +1,36 @@
+#requires -runasadministrator 
+#requires -modules ActiveDirectory 
+
+#
+# Paolo Frigo, https://www.scriptinglibrary.com
+# This script will sum all VM Footprint according to the filter PROD/TEST/DEV used.
+# 
+
+
+$Filter = "prod" # prod, test or dev
+$VMNODES = "VMNode-01", "VMNode-02", "VMNode-03" #get-adcomputer -filter {name -like "My_Naming_Convention"} | Select-object -expandproperty name
+$Result = 0
+
+foreach ($VMHost in $VMNODES) {
+    if (Test-Connection $VMHost -Quiet -Count 1) {
+        $VM = get-vm -computername $VMHost | Where-Object {$_.name -match "$Filter"}
+        $Disks = $VM | Get-VMHardDiskDrive | Select-object -expandproperty path
+        $total_size = Invoke-Command -ComputerName $VMHost -ScriptBlock {   
+            param($Disks) 
+            $total = 0 
+            #write-host $disks
+            foreach ($disk in $disks) {
+                $size = [math]::round((get-item -Path $disk | select-object -ExpandProperty length)/1GB,2)
+                write-host "Disk: $disk `t Size $size GB"
+                $total += $size
+            } 
+            return $total
+            } -ArgumentList (,$Disks)
+        $Result += $total_size
+     }
+    else {
+        raise "$VMHost is not reachable"
+    }
+}
+
+Write-Output "$Filter VM footprint is $Result GB".ToUpper()
