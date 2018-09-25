178786971c3e148bc2fd82afb4cb996e23ed23a4
Paolo Frigo <paolofrigo@gmail.com>
2018-09-25 13:29:16 +1000

    Script updated checking is gateway is already installed and other checks

--- Blog/PowerShell/Install-WAC.ps1
+++ Blog/PowerShell/Install-WAC.ps1
@@ -8,5 +8,27 @@
 
 $WAC_Online = "http://aka.ms/WACDownload"
 $WAC_Installer = "C:\windows\Temp\wac.msi"
+$Port = 443
+
+$IsAdminCenterInstalled = [bool] (Get-WmiObject -class win32_product  | Where-Object {$_.Name -eq "Windows Admin Center"})
+
+If ($IsAdminCenterInstalled){
+    $ReInstall = Read-Host "Admin Center is already installed do you want to re-install or upgrade it? [Y/N]"
+    If ( ("N","n") -contains $ReInstall){
+        Write-Warning "Windows Admin Center is already installed and no further action is required."   
+        Exit 0
+    }
+}
+
 Invoke-WebRequest -Uri $WAC_Online -OutFile $WAC_Installer
-msiexec /i $WAC_Installer /qn SME_PORT=443 SSL_CERTIFICATE_OPTION=generate
+msiexec /i $WAC_Installer /qn SME_PORT=$Port SSL_CERTIFICATE_OPTION=generate
+
+#Post Installation Checks
+do {
+    if ((Get-Service ServerManagementGateway).status -ne "Running"){
+        Write-Output "Starting for Windows Admin Center"
+        Start-Service ServerManagementGateway
+    }
+    Start-sleep -Seconds 5
+} until ((Test-NetConnection -ComputerName "localhost" -port $Port).TcpTestSucceeded)
+Write-Output "Installation completed and Windows Admin Center is running as expected."
