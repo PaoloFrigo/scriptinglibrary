85f1db2292fc22213d5a76a6f787ff6906d80f87
Paolo Frigo <paolofrigo@gmail.com>
2018-10-24 21:32:50 +1100

    Added Monitor-AccountLockout.ps1 script

---
+++ Blog/PowerShell/Monitor-AccountLockOut.ps1
@@ -0,0 +1,30 @@
+#requires -module BurntToast,ActiveDirectory
+
+#Paolo Frigo, https://www.scriptinglibrary.com
+
+$SleepTime = 60 #seconds
+do {
+    if (Search-ADAccount -LockedOut) {
+        foreach ($user in (Search-ADAccount -LockedOut)) {
+            New-Burnttoastnotification -text "Locked-Out User Notification", "$($($user).name) is now Locked-Out"
+        } 
+    }       
+    start-sleep -Seconds $SleepTime
+}
+while ($True) 
+
+
+<#
+ # If you want a limited number of test you can use a foreach loop instead of a do-while 
+ # 
+ForEach ($i in (1..100)){
+    if (Search-ADAccount -LockedOut) {
+        foreach ($user in (Search-ADAccount -LockedOut)) {
+            New-Burnttoastnotification -text "Locked-Out User Notification", "$($($user).name) is now Locked-Out"
+        } 
+    }       
+    start-sleep -Seconds $SleepTime
+}
+#>
+
+ # A better approach can be simple schedule this job and run it any minute.
