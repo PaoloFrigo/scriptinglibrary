ba7b68e37bd32f43a3a94ccbed650be782c8d109
Paolo Frigo <paolofrigo@gmail.com>
2018-10-29 22:02:30 +1100

    Install a Primary Domain Controller

---
+++ Blog/PowerShell/Install-DC.ps1
@@ -0,0 +1,10 @@
+#Paolo Frigo, https://www.scriptinglibrary.com
+
+# Create a AD DC with Powershell
+
+# Install the role, import the module, create a forest 
+# add this server as a Primary Domain Controller 
+
+install-windowsfeature AD-Domain-Services
+Import-Module ADDSDeployment
+Install-ADDSForest 
