0a677332661b7cfb441afe743a940f334509edc8
Paolo Frigo <paolofrigo@gmail.com>
2019-05-11 15:22:09 +1000

    New-ServiceAccount.ps1 script added

---
+++ Blog/PowerShell/New-ServiceAccount.ps1
@@ -0,0 +1,36 @@
+﻿#requires -module ActiveDirectory
+
+#Paolo Frigo, https://www.scriptinglibrary.com
+
+$CSVFILEPATH = "D:\Scripts\service_accounts.csv"
+$DEST_OU="OU=Service Accounts,DC=lab,DC=local"
+
+
+function New-ServiceAccount {
+    Param
+    (
+        [Parameter(Mandatory=$true)]
+        [string]
+        $samaccountname,
+         [Parameter(Mandatory=$true)]
+        [string]
+        $description,
+         [Parameter(Mandatory=$true)]
+        [SecureString]
+        $password,
+         [Parameter(Mandatory=$true)]
+        [string]
+         $destou
+    )
+    $psw = convertto-securestring "$password" -asplaintext -force
+    New-ADUser -Path $destou -Name "$samaccountname"  -AccountPassword $psw -Enabled $true -AllowReversiblePasswordEncryption $false -CannotChangePassword $true -PasswordNeverExpires $true
+    Write-Output "$samaccountname service account created in $destou"
+}
+
+
+if ((Test-path ($CSVFILEPATH)) -eq $false){
+    throw "CSV FILE $CSVFILEPATH not found!"
+}
+Foreach ($sa in $(import-csv -Path $CSVFILEPATH)){
+    New-ServiceAccount -samaccountname $sa.samaccountname -description $sa.description -password $sa.password -destou $DEST_OU
+}
