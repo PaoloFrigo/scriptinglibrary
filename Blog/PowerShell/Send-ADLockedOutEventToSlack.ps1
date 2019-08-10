0138b379739f6b026b695b60ef2462e7ced4894b
Paolo Frigo <paolofrigo@gmail.com>
2019-08-10 13:48:33 +1000

    Script edited and fixed with Invoke-ScriptAnalyser

--- Blog/PowerShell/Send-ADLockedOutEventToSlack.ps1
+++ Blog/PowerShell/Send-ADLockedOutEventToSlack.ps1
@@ -8,22 +8,22 @@
 # every single lockout-event in the local AD domain.
 
 # Webhooks Channel
-$SlackChannelUri = "https://hooks.slack.com/services/E/ETCETC"
+$SlackChannelUri = "https://hooks.slack.com/services/ETC..ETC.."
 $ChannelName = "#general"
 
 $BodyTemplate = @"
     {
-        "channel": "CHANNELNAME", 
-        "username": "AD-Mominitor", 
-        "text": "DOMAIN_USERNAME account is currently locked out. DATETIME.", 
-        "icon_emoji":":ghost:" 
+        "channel": "CHANNELNAME",
+        "username": "ActiveDirectory Bot",
+        "text": "*DOMAIN_USERNAME* account is currently locked out. \nTime: DATETIME.",
+        "icon_emoji":":ghost:"
     }
 "@
 
 
 if (Search-ADAccount -LockedOut){
     foreach ($user in (Search-ADAccount -LockedOut)){
-        $body = $BodyTemplate.Replace("DOMAIN_USERNAME","$user").Replace("DATETIME",$(Get-Date)).Replace("CHANNELNAME","$ChannelName")
+        $body = $BodyTemplate.Replace("DOMAIN_USERNAME","MyTestUsr01").Replace("DATETIME",$(Get-Date)).Replace("CHANNELNAME","$ChannelName")
         Invoke-RestMethod -uri $SlackChannelUri -Method Post -body $body -ContentType 'application/json'
     }
 }
