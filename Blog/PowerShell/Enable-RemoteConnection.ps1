4696ed2523c4b0e9f6e656d3db589c0594caf3e6
Paolo Frigo <paolofrigo@gmail.com>
2019-10-23 22:12:34 +1100

    mandatory parameter added and minor chages

--- Blog/PowerShell/Enable-RemoteConnection.ps1
+++ Blog/PowerShell/Enable-RemoteConnection.ps1
@@ -1,20 +1,29 @@
 #Paolo Frigo, https://www.scriptinglibrary.com
 
-#This scripts enable the remote connection to the target host, add a firewall rule and test if the port is open.
+# This script enables remote connection on the ComputerName host, add a firewall rule to the winodws host based firewall and 
+# tests if the standard RDP port is open.
 
 # If you want to provide local admin credentials
 # $cred = Get-Credential 
 
-$target = Read-host "Enter the target computername"
+Param (
+    [Parameter(Mandatory=$true)]    
+    $ComputerName 
+)
 
-if (test-connection -computername $target -quiet -count 1){
-    #Enable The remote desktop connections
-    invoke-command -computername $target -scriptblock {Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0} #-credential $cred
-    invoke-command -computername $target -scriptblock {Enable-NetFirewallRule -DisplayGroup "Remote Desktop"} #-credential $cred
-    if ((test-netconnection -computername $target -port 3389).TcpTestSucceeded -eq $True){
-        Write-Output "Remote connection ENABLED, Firewall rule ADDED and RDP port OPEN on $target"
+#Tests if the host is the ComputerName host is reachable
+if (test-connection -computername $ComputerName -quiet -count 1){
+    #Enables The remote desktop connections
+    invoke-command -computername $ComputerName -scriptblock {Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0} #-credential $cred
+    #Adds a firewall rule
+    invoke-command -computername $ComputerName -scriptblock {Enable-NetFirewallRule -DisplayGroup "Remote Desktop"} #-credential $cred
+    if ((test-netconnection -computername $ComputerName -port 3389).TcpTestSucceeded -eq $True){
+        Write-Output "Remote connection ENABLED, Firewall rule ADDED and RDP port OPEN on $ComputerName"
+    }
+    else{
+        Write-Warning "The RDP port on $ComputerName is not open"
     }
 }
 else {
-    Write-Warning "$target is not reachable"
+    Write-Warning "$ComputerName is not reachable"
 }
