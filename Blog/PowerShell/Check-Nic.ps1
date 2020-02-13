fd6360d5d1b96ff4cc8c19f63c1a864aa574b472
Paolo Frigo <paolofrigo@gmail.com>
2020-02-13 23:45:14 +1100

    Added the transferrate value

--- Blog/PowerShell/Check-Nic.ps1
+++ Blog/PowerShell/Check-Nic.ps1
@@ -1,20 +1,22 @@
 #Nagios/NSCP Network Interface Card Load Check
 #Author: Paolo Frigo, https://www.scriptinglibrary.com
-
 $NETWORK_CARD ="microsoft hyper-v network adapter*" 
+
 $WARNING = 29
 $CRITICAL = 49
 
-$NetworkUtilisation = [math]::round(((get-counter).countersamples  | where-object {$_.instancename -like "$NETWORK_CARD"} | select-object -exp CookedValue )*8/(1024*1024),2)
+$TransferRate = ((get-counter).countersamples  | where-object {$_.instancename -like "$NETWORK_CARD"} | select-object -exp CookedValue )*8
+
+$NetworkUtilisation = [math]::round($TransferRate/1000000000*100,2)
 
 if ($NetworkUtilisation -gt $CRITICAL){
-	Write-Output "CRITICAL: $($NetworkUtilisation) % Network utilisation"   
+	Write-Output "CRITICAL: $($NetworkUtilisation) % Network utilisation, $($TransferRate.ToString('N0')) b/s"   
 	exit 2
 }
 if ($NetworkUtilisation -gt $WARNING){
-	Write-Output "WARNING: $($NetworkUtilisation) % Network utilisation"   
+	Write-Output "WARNING: $($NetworkUtilisation) % Network utilisation, $($TransferRate.ToString('N0')) b/s"
 	exit 1
 }
-
-Write-Output "OK: $($NetworkUtilisation) % Network utilisation"   
+Write-Output "OK: $($NetworkUtilisation) % Network utilisation, $($TransferRate.ToString('N0')) b/s"   
 exit 0
+
