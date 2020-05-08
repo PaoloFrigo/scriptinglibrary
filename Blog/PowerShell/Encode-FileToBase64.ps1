444d880e5799cedb70b498d4dd33acff80e57992
Paolo Frigo <paolofrigo@gmail.com>
2020-05-08 10:10:00 +1000

    Replaced Get-Content with System.Io.File.ReadAllText to fix CRLF missing in CSV

--- Blog/PowerShell/Encode-FileToBase64.ps1
+++ Blog/PowerShell/Encode-FileToBase64.ps1
@@ -3,13 +3,13 @@
 #This helper function will encode a file into a Base64 string
 function Encode-FileToBase64($filename){
     if (Test-Path "$filename"){
-        $content = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($(get-content $fileName)))
+        $content = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes([System.IO.File]::ReadAllText("$fileName")))
         return $filename, $content
     }
     throw "File not found $fileName"
 }
 
 #EX
-ls > example.txt   
-Encode-FileToBase64 example.txt
+ls | export-csv example.csv
+Encode-FileToBase64 example.csv
 
