87ab0cb68288b059c99fde9d3346f33c38c273b7
Paolo Frigo <paolofrigo@gmail.com>
2020-05-08 16:31:58 +1000

    Decode-Base64ToFile Script added

---
+++ Blog/PowerShell/Decode-Base64ToFile.ps1
@@ -0,0 +1,24 @@
+#Paolo Frigo, https://www.scriptinglibrary.com
+
+#This helper function will dencode a base64 string into a fil
+function Decode-Base64ToFile{
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$True)]
+        [string] 
+        $outputfile,
+        [Parameter(Mandatory=$True)]
+        [string] 
+        $base64string
+    )
+    [System.IO.File]::WriteAllText($outputfile, [System.Text.Encoding]::Utf8.GetString([System.Convert]::FromBase64String($base64string)))
+    Write-Output "$($outputfile) created"
+}
+
+#[System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes([System.IO.File]::ReadAllText("hello.txt")))
+
+#Hello, World! 
+$base64 = "SGVsbG8sIFdvbHJkIQ0K"
+$outputfile = "hello.txt"
+
+Decode-Base64ToFile -outputfile $outputfile -base64string $base64
