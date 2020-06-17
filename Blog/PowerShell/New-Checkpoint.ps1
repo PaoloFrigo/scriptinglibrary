d4d597edee685ede5b5d299dd82fbbe04696e0e0
Paolo Frigo <paolofrigo@gmail.com>
2020-06-17 16:34:02 +1000

    2 Powershell scripts addded: New-Checkpoint and Restore-RecentCheckpoint

---
+++ Blog/PowerShell/New-Checkpoint.ps1
@@ -0,0 +1,11 @@
+﻿#requires -runasadministrator
+
+#requires -runasadministrator
+
+# Paolo Frigo, https://www.scriptinglibrary.com
+
+$prefix = "lab-centos*"
+
+Write-Output "This script will create a checkpoint immediately for all $prefix  VMs"
+get-vm $prefix | checkpoint-vm
+Write-Output "Activity completed"
---
+++ Blog/PowerShell/Restore-RecentChecktpoint.ps1
@@ -0,0 +1,10 @@
+﻿#requires -runasadministrator
+
+# Paolo Frigo, https://www.scriptinglibrary.com
+
+$prefix = "lab-centos*"
+
+
+Write-Output "This script will restore the most recent checkpoint for all $prefix VMs without asking for confirmation"
+Get-VM $prefix | Foreach-Object { $_ | Get-VMSnapshot | Sort CreationTime | Select -Last 1 | Restore-VMSnapshot -Confirm:$false }
+Write-Output "Activity completed"
