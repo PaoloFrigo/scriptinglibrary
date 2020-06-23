2421c453f62e7d06b530fa699271da3934532238
Paolo Frigo <paolofrigo@gmail.com>
2020-06-23 10:28:43 +1000

    pfx-utils script added

---
+++ Blog/Bash/pfx-utils.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# THIS SCRIPT EXTRACT A CERTIFICATE AND PRIVATE KEY FROM PFX FILE
+# Paolo Frigo, https://www.scriptinglibrary.com
+
+read -p "Certificate name (e.g. MyCert.pfx):" PfxCert
+CertName=${PfxCert/.pfx/}
+
+#Export the private key
+openssl pkcs12 -in $PfxCert -nocerts -out key.pem -nodes
+
+#Export the certificate
+openssl pkcs12 -in $PfxCert -nokeys -out $CertName.pem
+
+#Remove the passphrase from the private key
+openssl rsa -in key.pem -out $CertName.key
+
+echo "$CertName.pem, key.pem and $CertName.key generated from $PfxCert"
