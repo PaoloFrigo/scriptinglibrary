a105058958840052e1d326d37f73e5d893a60099
Paolo Frigo <paolofrigo@gmail.com>
2020-09-13 08:46:32 +1000

    Added an old version of a script to compress log files.

---
+++ Blog/PowerShell/compress-log.ps1
@@ -0,0 +1,49 @@
+﻿#requires -runasadministrator
+
+# Paolo Frigo https://www.scriptinglibrary.compress
+
+# List of Log Folders
+$Logs =  "D:\Logs\W3SVC6", "D:\Logs\W3SVC5", "D:\Logs\W3SVC4", "D:\Logs\W3SVC3", "D:\Logs\W3SVC2"
+
+
+$oldpath = $pwd
+
+function compress-log {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]
+        $LogDir
+    )
+
+
+    Set-Location $LogDir
+    $Logs = $(Get-ChildItem *log)
+
+
+    $counter = 1
+    foreach ($log in $Logs){
+        Write-Progress -Activity "Activity: Compressing $log" -Status "Compressing $log" -PercentComplete "$($counter/($logs.count)*100)"
+
+
+        $Compress=@{
+        Path = "$log"
+        DestinationPath= "$log.zip"
+        CompressionLevel = "Fastest"
+        }
+        Compress-Archive @Compress
+        if (Test-Path $Compress.DestinationPath){
+            Remove-Item $log
+        }
+        $counter = $counter + 1
+    }
+}
+
+
+foreach ($LogFolder in $Logs){
+    if (Test-Path $LogFolder){
+        Write-Output "STARTING TO COMPRESS LOGS IN $LogFolder"
+        compress-log -LogDir $LogFolder
+    }
+}
+Set-Location $oldpath
