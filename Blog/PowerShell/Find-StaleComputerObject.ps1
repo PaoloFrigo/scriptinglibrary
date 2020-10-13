1f0bb64694eb1703f2ccbbe0cb91e4858e8735db
Paolo Frigo <paolofrigo@gmail.com>
2020-10-13 13:59:29 +1100

    This script creates a report of all stale computer objects in AD

---
+++ Blog/PowerShell/Find-StaleComputerObject.ps1
@@ -0,0 +1,23 @@
+#requires -module ActiveDirectory
+
+# Paolo Frigo, https://www.scriptinglibrary.com
+
+# This scripts creates a repot of all the Computer Objects in AD, 
+# without limiting the result to a specific OU, that are not logged-in 
+# since N days ago.
+
+$NumberOfDays   =   200
+$ReportName     =   "StaleObjReport.csv"
+
+#Saves the report on the same dir of this script
+$ReportNamePath = "$PSScriptRoot\\$ReportName" 
+$InactivityPeriod = (Get-Date).Adddays(-($NumberOfDays))
+
+$StaleObjectSearch = @{
+    Filter = {LastLogonTimeStamp -lt $InactivityPeriod}
+    ResultPageSize = 2000 
+    resultSetSize =  $null
+    Properties = "Name", "OperatingSystem", "SamAccountName", "DistinguishedName"
+}
+
+Get-ADComputer @StaleObjectSearch | Export-Csv -NoTypeInformation -Path $ReportNamePath  
