f6743995b7a840cee30acca81736b8782d406623
Paolo Frigo <paolofrigo@gmail.com>
2020-10-15 11:50:11 +1100

    typo fixed int he description

--- Blog/Python/urlfromsitemap.py
+++ Blog/Python/urlfromsitemap.py
@@ -5,7 +5,7 @@ import xml.etree.ElementTree as ET
 #Paolo Frigo, https://www.scriptinglibrary.com 
 
 parser = argparse.ArgumentParser(
-    description='THIS SCRIPTS PRINTS ALL URL IN THE SITEMAP.XML FILE',
+    description='THIS SCRIPTS PRINTS ALL URLs IN THE SITEMAP.XML FILE',
     usage="./urlfromsitemap.py sitemap.xml ", epilog='Author: Paolo Frigo')
         
 parser.add_argument('sitemap', default='sitemap.xml', help='sitemap.xml')
