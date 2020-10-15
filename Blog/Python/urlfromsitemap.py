9ccd0d04cfa1b69d683d0bc39de5e58e9bfe9306
Paolo Frigo <paolofrigo@gmail.com>
2020-10-15 11:42:30 +1100

    new utility to extract url from sitemap.xml

---
+++ Blog/Python/urlfromsitemap.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import argparse, logging
+import xml.etree.ElementTree as ET
+
+#Paolo Frigo, https://www.scriptinglibrary.com 
+
+parser = argparse.ArgumentParser(
+    description='THIS SCRIPTS PRINTS ALL URL IN THE SITEMAP.XML FILE',
+    usage="./urlfromsitemap.py sitemap.xml ", epilog='Author: Paolo Frigo')
+        
+parser.add_argument('sitemap', default='sitemap.xml', help='sitemap.xml')
+args = parser.parse_args()
+
+try: 
+    tree = ET.parse(args.sitemap)
+    root = tree.getroot()
+
+    for child in root:
+        print (child[0].text)
+
+except:
+    logging.error("Failed to process " + args.sitemap, exc_info=True)
+    exit(1)
