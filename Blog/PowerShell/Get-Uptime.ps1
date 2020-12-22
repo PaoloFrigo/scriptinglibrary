4e4c9e95af8248feaf2846a72c88ede915f7188f
Paolo Frigo <paolofrigo@gmail.com>
2020-12-22 14:58:44 +1100

    Added -exp

--- Blog/PowerShell/Get-Uptime.ps1
+++ Blog/PowerShell/Get-Uptime.ps1
@@ -1,4 +1,4 @@
 #
 #   Paolo Frigo, www.scriptinglibrary.com
 #
-Get-CimInstance -ClassName win32_operatingsystem | Select-Object LastBootUpTime
+Get-CimInstance -ClassName win32_operatingsystem | Select-Object -ExpandProperty LastBootUpTime
