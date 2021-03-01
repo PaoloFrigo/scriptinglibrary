ff7e5167b10550dcea332d2239b263d278a9fd1e
Paolo Frigo <paolofrigo@gmail.com>
2021-03-01 12:06:37 +1100

    Description Changed for New-Virus oneliner script.

--- Blog/PowerShell/New-Virus.ps1
+++ Blog/PowerShell/New-Virus.ps1
@@ -1,6 +1,6 @@
 #Paolo Frigo, https://scriptinglibrary.com
 
-# This script creates a VIRUS file for testing AV soultions
+# This script creates a COM file containing a Virus for testing AV soultions
 
 #https://en.wikipedia.org/wiki/EICAR_test_file
 set-content "X5O!P%@AP[4`\PZX54(P^)7CC)7}`$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!`$H+H*" -path "eicar.com"
