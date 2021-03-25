8bb8a387889486e6e9fee1d3c518bb19b41fe293
Paolo Frigo <paolofrigo@gmail.com>
2021-03-26 10:11:31 +1100

    Added a new snippet for using robocopy

---
+++ Blog/Batch/robocopy-clonesharefolder.bat
@@ -0,0 +1,10 @@
+Rem Paolo Frigo, https://scriptinglibrary.com
+
+Rem The original author of thi snippet is CRAIG SCHELLENBERG
+Rem https://www.sikich.com/insight/how-to-perform-file-server-migrations-using-robocopy-part-1/
+
+Rem REPLACE
+Rem <SOURCE>, <DESTINATION> AND <LOGNAME> LOGNAME_%date:~-10,2%”-“%date:~7,2%”-“%date:~-4,4%.txt
+
+robocopy <SOURCE> <DESTINATION> /e /b /copyall /PURGE /r:5 /w:5 /MT:64 /tee /log+:<LOGNAME> /v
+
