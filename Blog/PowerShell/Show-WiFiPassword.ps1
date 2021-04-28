8213df671d913bc2aba858910f0989b5d24a363e
Paolo Frigo <paolofrigo@gmail.com>
2021-04-28 20:19:03 +1000

    Show-WiFiPassword script added

---
+++ Blog/PowerShell/Show-WiFiPassword.ps1
@@ -0,0 +1,26 @@
+﻿#Paolo Frigo, https://scriptinglibrary.com
+
+# This script print all WiFi setting stored in the your system
+# SSID : passphrase
+
+# .Notes
+# This script is a wrapper of this 2 netsh commands
+# netsh.exe wlan show profiles                              #Shows all your wifi
+# netsh.exe wlan show profiles name="$SSID" key=clear       #Shows the passphrase stored for SSID wifi network in the Key Content value.
+
+$wifi = $(netsh.exe wlan show profiles)
+
+$ListOfSSID = ($wifi | Select-string -pattern "\w*All User Profile.*: (.*)" -allmatches).Matches | ForEach-Object {$_.Groups[1].Value}
+$NumberOfWifi = $ListOfSSID.count
+Write-Warning "[$(Get-Date)] I've found $NumberOfWifi Wi-Fi Connection settings stored in your system $($env:computername) : "
+foreach ($SSID in $ListOfSSID){
+    try {
+        $passphrase = ($(netsh.exe wlan show profiles name=`"$SSID`" key=clear) |
+                    Select-String -pattern ".*Key Content.*:(.*)" -allmatches).Matches |
+                        ForEach-Object {$_.Groups[1].Value}
+    }
+    catch {
+        $passphrase = "N/A"
+    }
+    Write-Output "$SSID : $passphrase"
+}
