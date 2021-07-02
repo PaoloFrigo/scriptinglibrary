9631f57a086841a3ed7c89868fadd135d312f82f
Paolo Frigo <paolofrigo@gmail.com>
2021-07-03 00:58:15 +1000

    Add-FileToBlobStorage script added

---
+++ Blog/PowerShell/Add-FileToBlobStorage.ps1
@@ -0,0 +1,39 @@
+﻿#Paolo Frigo, https://scriptinglibrary.com
+
+<#
+    .SYNOPSIS
+    Add a file to a blob storage
+
+    .DESCRIPTION
+    This function leverages the Azure Rest API to upload a file into a blob storage using a SAS token.
+
+    .PARAMETER file
+    Absolute path of the file to upload
+
+    .PARAMETER connectionstring
+    Uri and SAS token
+
+    .EXAMPLE
+    Add-FileToBlogStorage -file "FULL_PATH" -connectionstring "BLOBSTORAGE_URI_WITH_SAS_TOKEN"
+
+#>
+function Add-FileToBlobStorage{
+    Param(
+        [Parameter(Mandatory=$true)]
+        [ValidateScript({Test-Path $_ })]
+        [string]
+        $file,
+        [Parameter(Mandatory=$true)]
+        [ValidateScript({$_ -match "https" -and $_ -match "\?"})]
+        [string]
+        $connectionstring
+    )
+    $HashArguments = @{
+        uri = $connectionstring.replace("?","/$($(get-item $file).name)?")
+        method = "Put"
+        InFile = $file
+        headers = @{"x-ms-blob-type" = "BlockBlob"}
+
+    }
+    Invoke-RestMethod @HashArguments
+}
